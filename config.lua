3d9b289b74aa12ae8f9dcc83b266fee37e3ebfc7
WOLF STUDIO DEVELOPMENTS <123537406+WOLFALEN@users.noreply.github.com>
2023-06-10 14:30:17 +0530

    config.lua

--- config.lua
+++ config.lua
@@ -6,6 +6,6 @@ Config.shrinkRate = vector3(0.01, 0.01, 0.01)
 
 Config.markerProperties = {
     type = 1,
-    size = vector3(1200.001, 1200.0001, 600.5001),
+    radius = 100.5001,
     color = { r = 0, g = 208, b = 255, a = 200 }
 }
