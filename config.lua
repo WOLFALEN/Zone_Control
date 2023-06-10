2738dc8b0e43cf611bbec9fc5b3ae5fa3a5ab642
WOLF STUDIO DEVELOPMENTS <123537406+WOLFALEN@users.noreply.github.com>
2023-06-10 14:02:37 +0530

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,11 @@
+Config = {}
+
+Config.zone = vector3(4903.2974, -4918.4976, 3.3862)
+
+Config.shrinkRate = vector3(0.01, 0.01, 0.01)
+
+Config.markerProperties = {
+    type = 1,
+    size = vector3(1200.001, 1200.0001, 600.5001),
+    color = { r = 0, g = 208, b = 255, a = 200 }
+}
